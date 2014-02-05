03b517345078ab86345d09a473ce45f5acb77b27
Darren Stahl <darren@mx.stahlsolutions.ca>
2014-02-05 10:26:41 +0400

    Fixing compile errors

--- tests/check_lock_free_queue.c
+++ tests/check_lock_free_queue.c
@@ -2,17 +2,6 @@
 #include <check.h>
 #include "../src/lock_free_queue.h"
 
-void
-setup (void)
-{
-}
-
-void
-teardown (void)
-{
-}
-
-START_TEST(test_blank) {
-    int i = 0;
-    ck_assert_int_eq (i, 0);
+int main() {
+    return 0;
 }
